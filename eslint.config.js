27ff21fdcb26d0e3f4fc4270156d9c01173d4735
wsvaio <wsvaio@qq.com>
2023-12-26 14:31:56 +0800

    fix: eslint-config 给userConfig加上类型

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 // eslint.config.js
 import wsvaio from "@wsvaio/eslint-config";
 
-// import {vue} from "@wsvaio/eslint-config/overwrites"
 
 export default wsvaio();
---
+++ packages/eslint-config/userConfigs.d.ts
@@ -0,0 +1,3 @@
+import type { UserConfigItem } from "@antfu/eslint-config";
+
+export const vue: UserConfigItem;
